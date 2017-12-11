4e07d53f4753b6a78e5e0460f373030308257767
Leon van der Houven <leon.metal.1994@live.nl>
2017-12-11 22:43:45 +0100

    role table aangepast

--- database/migrations/2017_12_08_092737_Role.php
+++ database/migrations/2017_12_08_092737_Role.php
@@ -16,7 +16,6 @@ class Role extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->timestamps();
         });
     }
 
